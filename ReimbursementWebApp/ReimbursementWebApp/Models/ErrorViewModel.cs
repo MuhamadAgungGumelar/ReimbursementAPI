4f6e3977eabdc74a4c1952160b9a5f1b84baee7e
MuhamadAgungGumelar <mohamadagung007@gmail.com>
2023-10-23 10:22:12 +0700

    Add project files.

---
+++ ReimbursementWebApp/ReimbursementWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReimbursementWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
