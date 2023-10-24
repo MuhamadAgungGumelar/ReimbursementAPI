6178b814f3371583d3d91f540fd2fee0fb7168cd
MuhamadAgungGumelar <mohamadagung007@gmail.com>
2023-10-24 10:31:52 +0700

    feature: adding login page

--- ReimbursementWebApp/ReimbursementWebApp/Controllers/HomeController.cs
+++ ReimbursementWebApp/ReimbursementWebApp/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace ReimbursementWebApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         public IActionResult LandingPage()
         {
             return View();
