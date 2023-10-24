3613a5693e7437bb64706d46275958b6b7635c53
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-24 10:13:51 +0700

    feature: add Role Models

---
+++ ReimbursementAPI/Models/Roles.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReimbursementAPI.Models
+{
+    [Table("tb_m_roles")]
+    public class Roles : BaseEntity
+    {
+        [Column("name", TypeName = "varchar(100)")]
+        public string Name { get; set; }
+    }
+}
