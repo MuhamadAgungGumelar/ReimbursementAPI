908d4224c37e259f59078f5aef776a34a1570200
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-24 10:13:36 +0700

    feature: add Finance Models

---
+++ ReimbursementAPI/Models/Finances.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReimbursementAPI.Models
+{
+    [Table("tb_m_finances")]
+    public class Finances : BaseEntity
+    {
+        [Column("employee_guid")]
+        public Guid EmployeeGuid { get; set; }
+        [Column("name", TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+        [Column("approved_date", TypeName = "datetime2")]
+        public DateTime ApprovedDate { get; set; }
+        [Column("approved_amount", TypeName = "int")]
+        public int ApprovedAmount { get; set; }
+    }
+}
