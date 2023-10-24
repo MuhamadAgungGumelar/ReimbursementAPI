cd756cb607ceeddbd89a9b7c54655f53d55e58b7
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-24 15:44:45 +0700

    feature: adding Finance repo

---
+++ ReimbursementAPI/Repository/FinanceRepository.cs
@@ -0,0 +1,13 @@
+﻿using ReimbursementAPI.Contracts;
+using ReimbursementAPI.Data;
+using ReimbursementAPI.Models;
+
+namespace ReimbursementAPI.Repository
+{
+    public class FinanceRepository : GeneralRepository<Finances>, IFinanceRepository
+    {
+        public FinanceRepository(ReimbursementDBContext context) : base(context)
+        {
+        }
+    }
+}
