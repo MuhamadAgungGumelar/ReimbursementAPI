6fce4d946b4881466f60e3b8a59a953031837b19
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-24 10:12:46 +0700

    feature: add AccountRole Models

---
+++ ReimbursementAPI/Models/AccountRoles.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReimbursementAPI.Models
+{
+    [Table("tb_m_account_roles")]
+    public class AccountRoles : BaseEntity
+    {
+        [Column("account_guid")]
+        public Guid AccountGuid { get; set; }
+        [Column("role_guid")]
+        public Guid RoleGuid { get; set; }
+    }
+}
