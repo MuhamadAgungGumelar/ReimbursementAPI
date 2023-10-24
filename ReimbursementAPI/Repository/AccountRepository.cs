8716705f255c4acf7368fa9e58a638dcc3acc64f
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-24 15:44:19 +0700

    feature: adding Account repo

---
+++ ReimbursementAPI/Repository/AccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using ReimbursementAPI.Contracts;
+using ReimbursementAPI.Data;
+using ReimbursementAPI.Models;
+
+namespace ReimbursementAPI.Repository
+{
+    public class AccountRepository : GeneralRepository<Accounts>, IAccountRepository
+    {
+        public AccountRepository(ReimbursementDBContext context) : base(context)
+        {
+        }
+    }
+}
