e841a2fd8de3475837a4323e3450800257431db3
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-24 10:12:53 +0700

    feature: add Account Models

---
+++ ReimbursementAPI/Models/Accounts.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReimbursementAPI.Models
+{
+    [Table("tb_m_accounts")]
+    public class Accounts : BaseEntity
+    {
+        [Column("password", TypeName = "nvarchar(100)")]
+        public string Password { get; set; }
+        [Column("otp", TypeName = "int")]
+        public int Otp { get; set; }
+        [Column("is_used", TypeName = "int")]
+        public bool IsUsed { get; set; }
+        [Column("expired_time", TypeName = "datetime2")]
+        public DateTime ExpiredTime { get; set; }
+    }
+}
