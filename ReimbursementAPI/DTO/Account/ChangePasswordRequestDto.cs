436934ba38c977a83d2b23bba490928be8b8b00c
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-25 17:35:30 +0700

    feature: create ChangePasswordDto

---
+++ ReimbursementAPI/DTO/Account/ChangePasswordRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ReimbursementAPI.DTO.Account
+{
+    public class ChangePasswordRequestDto
+    {
+        public string Email { get; set; }
+        public int Otp { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
