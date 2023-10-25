4172de1a27d4de75830f06b4268f9662a78bfaff
MuhamadAgungGumelar <mohamadagung007@gmail.com>
2023-10-25 09:22:57 +0700

    feature: adding Login and Profil Page

--- ReimbursementWebApp/ReimbursementWebApp/Controllers/HomeController.cs
+++ ReimbursementWebApp/ReimbursementWebApp/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace ReimbursementWebApp.Controllers
             return View();
         }
 
+        public IActionResult ProfilePage()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
