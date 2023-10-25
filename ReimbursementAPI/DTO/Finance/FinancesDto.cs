111daaa0ca1c2ba9340c210f9ad652d5ac46e60c
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-25 17:36:45 +0700

    feature: create FinancesDto

---
+++ ReimbursementAPI/DTO/Finance/FinancesDto.cs
@@ -0,0 +1,37 @@
+﻿using ReimbursementAPI.Models;
+
+namespace ReimbursementAPI.DTO.Finance
+{
+    public class FinancesDto : GeneralDto
+    {
+        public Guid EmployeeGuid { get; set; }
+        public string Name { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedAmount { get; set; }
+
+        public static explicit operator FinancesDto(Finances finances)
+        {
+            return new FinancesDto
+            {
+                Guid = finances.Guid,
+                EmployeeGuid = finances.EmployeeGuid,
+                Name = finances.Name,
+                ApprovedDate = finances.ApprovedDate,
+                ApprovedAmount = finances.ApprovedAmount,
+            };
+        }
+
+        public static implicit operator Finances(FinancesDto financesDto)
+        {
+            return new Finances
+            {
+                Guid = financesDto.Guid,
+                EmployeeGuid = financesDto.EmployeeGuid,
+                Name = financesDto.Name,
+                ApprovedDate = financesDto.ApprovedDate,
+                ApprovedAmount = financesDto.ApprovedAmount,
+                ModifiedDate = DateTime.Now
+            };
+        }
+    }
+}
