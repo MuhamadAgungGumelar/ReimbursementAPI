9c61d6596826d59e7d4fe2c15f1e3a1d5e75df19
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-25 17:36:01 +0700

    feature: create AccountRolesDto

---
+++ ReimbursementAPI/DTO/AccountRole/AccountRolesDto.cs
@@ -0,0 +1,31 @@
+﻿using ReimbursementAPI.Models;
+
+namespace ReimbursementAPI.DTO.AccountRole
+{
+    public class AccountRolesDto : GeneralDto
+    {
+        public Guid AccountGuid { get; set; } //deklarasi property
+        public Guid RoleGuid { get; set; } //deklarasi property
+
+        public static explicit operator AccountRolesDto(AccountRoles accountRole) //implementasi explicit Operator
+        {
+            return new AccountRolesDto
+            {
+                Guid = accountRole.Guid,
+                AccountGuid = accountRole.AccountGuid,
+                RoleGuid = accountRole.RoleGuid
+            };
+        }
+
+        public static implicit operator AccountRoles(AccountRolesDto accountRolesDto) //implementasi implicit Operator
+        {
+            return new AccountRoles
+            {
+                Guid = accountRolesDto.Guid,
+                AccountGuid = accountRolesDto.AccountGuid,
+                RoleGuid = accountRolesDto.RoleGuid,
+                ModifiedDate = DateTime.Now,
+            };
+        }
+    }
+}
