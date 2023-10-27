836f15beca562cb08c1e7f31152f6871a2145460
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-27 15:06:04 +0700

    feature: creating Token Handler Interface

---
+++ ReimbursementAPI/Contracts/ITokenHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace ReimbursementAPI.Contracts
+{
+    public interface ITokenHandler
+    {
+        string GenerateToken(IEnumerable<Claim> claims);
+    }
+}
