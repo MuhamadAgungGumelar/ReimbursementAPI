67c079186005516adfd72a409642316ccd4b08c9
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-27 15:02:14 +0700

    feature: adding register account dto

---
+++ ReimbursementAPI/DTO/Account/RegisterAccountResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ReimbursementAPI.DTO.Account
+{
+    public class RegisterAccountResponseDto
+    {
+        public string Email { get; set; }
+        public int Otp { get; set; }
+
+    }
+}
