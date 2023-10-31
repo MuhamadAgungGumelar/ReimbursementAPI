d42ea2bb6ef09b9caad84f5b8dce5c0b0362446c
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-31 18:19:18 +0700

    feature: implement CreateReimbursementViewModel

---
+++ ReimbursementWebApp/ReimbursementWebApp/Models/CreateReimburseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ReimbursementAPI.Utilities.Enums;
+
+namespace ReimbursementWebApp.Models
+{
+    public class CreateReimburseViewModel
+    {
+        public Guid EmployeeGuid { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Value { get; set; }
+        public StatusLevel Status { get; set; }
+        public IFormFile ImageFile { get; set; }
+    }
+}
