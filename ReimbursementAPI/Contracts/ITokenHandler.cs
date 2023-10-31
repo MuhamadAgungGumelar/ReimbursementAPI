78ffce5379b0397d2596ad62452e6e4b5ba40c54
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-11-01 01:22:42 +0700

    feature: creating DecodeToken Function

--- ReimbursementAPI/Contracts/ITokenHandler.cs
+++ ReimbursementAPI/Contracts/ITokenHandler.cs
@@ -1,9 +1,11 @@
-﻿using System.Security.Claims;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace ReimbursementAPI.Contracts
 {
     public interface ITokenHandler
     {
         string GenerateToken(IEnumerable<Claim> claims);
+        public JwtSecurityToken? DecodeToken(string token);
     }
 }
