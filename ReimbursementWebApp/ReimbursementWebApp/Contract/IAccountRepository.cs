c2edfe09d6e42259337d174b58bf791a273f694b
MuhamadAgungGumelar <mohamadagung007@gmail.com>
2023-11-01 22:31:17 +0700

    feature: implementing Login Authentication

---
+++ ReimbursementWebApp/ReimbursementWebApp/Contract/IAccountRepository.cs
@@ -0,0 +1,11 @@
+﻿using ReimbursementAPI.DTO.Account;
+using ReimbursementAPI.Utilities.Handler;
+using ReimbursementWebApp.Models;
+
+namespace ReimbursementWebApp.Contract
+{
+    public interface IAccountRepository : IRepository<AccountsDto, Guid>
+    {
+        Task<ResponseOKHandler<TokenDto>> Login(AccountLoginDto loginAccountDto);
+    }
+}
--- ReimbursementWebApp/ReimbursementWebApp/Controllers/AuthController.cs
+++ ReimbursementWebApp/ReimbursementWebApp/Controllers/AuthController.cs
@@ -1,12 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ReimbursementAPI.DTO.Account;
+using ReimbursementWebApp.Contract;
 
 namespace ReimbursementWebApp.Controllers
 {
     public class AuthController: Controller
     {
+        private readonly IAccountRepository _accountRepository;
+        //private readonly ITokenHandler _tokenService;
+
+        public AuthController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+            //_tokenService = tokenService;
+        }
+
         public IActionResult LoginPage()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginPage(AccountLoginDto loginAccountDto)
+        {
+            // Kirim permintaan login ke server API menggunakan IAuthRepository
+            var result = await _accountRepository.Login(loginAccountDto);
+
+            if (result.Status == "OK")
+            {
+                HttpContext.Session.SetString("JWToken", result.Data.Token);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View(loginAccountDto);
+            }
+        }
+
+        [HttpGet("Logout/")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("LoginPage", "Auth");
         }
     }
 }
