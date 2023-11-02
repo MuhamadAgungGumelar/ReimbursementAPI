375327ad3a63bde74660e3cf40cce7888d5342da
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-11-03 00:12:16 +0700

    fix: fixing update status bug

--- ReimbursementWebApp/ReimbursementWebApp/wwwroot/js/tableManagerReimbursement.js
+++ ReimbursementWebApp/ReimbursementWebApp/wwwroot/js/tableManagerReimbursement.js
@@ -35,10 +35,10 @@ $(document).ready(function () {
                     };
 
                     // Membuat select option
-                    return `<select class="form-select w-45" id="status_id">
+                    return `<select class="form-select w-45" id="status_id${meta.row}">
                               <option value="0"${data === 0 ? " selected" : ""}>waiting_manager_approval_reimburse</option>
                               <option value="1"${data === 1 ? " selected" : ""}>waiting_finance_approval_reimburse</option>
-                              <option value="2"${data === 3 ? " selected" : ""}>reimburse_rejected_by_manager</option>
+                              <option value="3"${data === 3 ? " selected" : ""}>reimburse_rejected_by_manager</option>
                             </select>`;
                 }
             },
@@ -53,7 +53,7 @@ $(document).ready(function () {
             {
                 data: null,
                 render: function (data, type, row, meta) {
-                    return `<button class="btn btn-link text-info text-sm mb-0 px-0 ms-0 approve-manager" onclick="update('${data.guid}')" data-id="${row.id}"><i class="ni ni-send"></i></button>`;
+                    return `<button class="btn btn-link text-info text-sm mb-0 px-0 ms-0 approve-manager" onclick="update('${data.guid}','${meta.row}')" data-id="${row.id}"><i class="ni ni-send"></i></button>`;
                 }
             },
         ]
@@ -61,7 +61,7 @@ $(document).ready(function () {
 
 });
 
-function update(guid) {
+function update(guid,row) {
     const token = $("#token").data("token");
     let data
     $.ajax({
@@ -82,7 +82,8 @@ function update(guid) {
         console.log(error);
     })
 
-    data.status = parseInt($("#status_id").val());
+    data.status = parseInt($("#status_id"+row).val());
+    console.log(data)
 
     $.ajax({
         url: "https://localhost:7257/api/Reimbursement/",
