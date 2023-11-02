35cd37ae7ff3bb3ae87893bc14218d64f1441c15
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-11-02 10:02:09 +0700

    fix: fixing validation for statuslevel

--- ReimbursementAPI/Utilities/Validators/Reimbursement/CreateReimbursementValidator.cs
+++ ReimbursementAPI/Utilities/Validators/Reimbursement/CreateReimbursementValidator.cs
@@ -13,7 +13,7 @@ namespace ReimbursementAPI.Utilities.Validators.Reimbursement
             RuleFor(e => e.Value).NotEmpty();
             RuleFor(e => e.ImageType).NotEmpty();
             RuleFor(e => e.Image).NotEmpty();
-            RuleFor(e => e.Status).NotEmpty().IsInEnum();
+            RuleFor(e => e.Status).NotNull().IsInEnum();
         }
     }
 }
